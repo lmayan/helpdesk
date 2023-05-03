a9b8a512079050c02761258b96c7e22df0878fdd
lmayan <lucasmayanae@gmail.com>
2023-05-03 16:18:06 -0300

    (DTO) create TecnicoDTO

--- src/main/java/com/app/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/app/helpdesk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.helpdesk.domain.Tecnico;
+import com.app.helpdesk.domain.dtos.TecnicoDTO;
 import com.app.helpdesk.services.TecnicoService;
 
 @RestController
@@ -18,9 +19,9 @@ public class TecnicoResource {
 	private TecnicoService tecnicoService;
 	
 	@GetMapping("/{id}")
-	private ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+	private ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
 		Tecnico obj = tecnicoService.findById(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 	
 }
