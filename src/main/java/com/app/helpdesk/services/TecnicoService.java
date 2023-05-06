ef2bdfae542e186dbcc23d878c462275e5c76b45
lmayan <lucasmayanae@gmail.com>
2023-05-06 15:39:54 -0300

    create personal ObjectNotFoundException

--- src/main/java/com/app/helpdesk/services/TecnicoService.java
+++ src/main/java/com/app/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.app.helpdesk.domain.Tecnico;
 import com.app.helpdesk.repositories.TecnicoRepository;
+import com.app.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,7 +17,7 @@ public class TecnicoService {
 
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = tecnicoRepository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Tecnico not found. (ID:"+id+")"));
 	}
 
 }
